52ce00688e0bea5acd2dc332792048c0da69098c
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-10-21 22:57:40 -0400

    example tests

---
+++ frontend/src/ExampleButton.tsx
@@ -0,0 +1,12 @@
+// src/components/Button.tsx
+import React from 'react';
+
+interface ButtonProps {
+  label: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ label }) => {
+  return <button>{label}</button>;
+};
+
+export default Button;
